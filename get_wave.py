8ac54d2e440f948ba9052b36208af06e15b7f39e
YoshikazuFusayasu <50490024+YoshikazuFusayasu@users.noreply.github.com>
2019-10-03 21:37:26 +0900

    Add files via upload

--- get_wave.py
+++ get_wave.py
@@ -3,6 +3,13 @@ import soundfile as sf
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import fftpack
+from scipy import signal
+
+import statistics
+import math
+
+import codecs
+import collections
 
 if __name__ == '__main__':
     in_file = "hoge.wav"
@@ -16,9 +23,10 @@ if __name__ == '__main__':
         freqList = fftpack.fftfreq(44100, d=1.0/ rate)
         
         amplitude = [np.sqrt(c.real ** 2 + c.imag ** 2) for c in X]
+        print(amplitude.index(max(amplitude)))  #1番大きい振幅を拾ってるだけ(雑音出たら、たぶん上手くいかない)
         
         plt.plot(freqList, amplitude, marker=".", linestyle="-", label = "fft plot")
-        plt.axis([0, 2000, 0, 2000])
+        plt.axis([0, 2000, 0, 7000])
         plt.xlabel("frequency [Hz]")
         plt.ylabel("amplitude")
         
