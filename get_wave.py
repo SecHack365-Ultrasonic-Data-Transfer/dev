2eebcd31c6cece8b939075856a330f7c287b2b86
YoshikazuFusayasu <50490024+YoshikazuFusayasu@users.noreply.github.com>
2019-10-03 20:09:03 +0900

    Add files via upload

---
+++ get_wave.py
@@ -0,0 +1,25 @@
+# - coding: utf-8 -
+import soundfile as sf
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import fftpack
+
+if __name__ == '__main__':
+    in_file = "hoge.wav"
+    
+    data, rate = sf.read(in_file)
+    
+    in_sec = len(data) / rate
+    for i in range(int(in_sec)):
+        print(i)
+        X = fftpack.fft(data[i*44100:(i+1)*44100])
+        freqList = fftpack.fftfreq(44100, d=1.0/ rate)
+        
+        amplitude = [np.sqrt(c.real ** 2 + c.imag ** 2) for c in X]
+        
+        plt.plot(freqList, amplitude, marker=".", linestyle="-", label = "fft plot")
+        plt.axis([0, 2000, 0, 2000])
+        plt.xlabel("frequency [Hz]")
+        plt.ylabel("amplitude")
+        
+        plt.show()
