38f1f82dc2c5435922b71a280b5749f0c72aa00a
katsuhikotakaishi <ichihiko999@gmail.com>
2019-10-04 16:49:08 +0900

    air

--- get_wave.py
+++ get_wave.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import fftpack
 from scipy import signal
+import pyaudio
+import wave
 
 import math
 
@@ -11,17 +13,58 @@ freq_std = 440
 
 
 if __name__ == '__main__':
-    in_file = "hoge.wav"
+    #in_file = "hoge.wav"
     
-    data, rate = sf.read(in_file)
+    #data, rate = sf.read(in_file)
     
     in_code = ""
     in_txt = ""
     
-    in_sec = len(data) / rate
+
+    chunk = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    RECORD_SECONDS = 3
+    WAVE_OUTPUT_FILENAME = "sample.wav"
+
+    p = pyaudio.PyAudio()
+    stream = p.open(
+        format = FORMAT,
+        channels = CHANNELS,
+        rate = RATE,
+        input = True,
+        frames_per_buffer = chunk
+    )
+
+    all = []
+    for i in range(0, int(RATE / chunk * RECORD_SECONDS)):
+        data = stream.read(chunk)
+        all.append(data)
+        try:
+            print(int(i / 50))
+            pass
+        except ZeroDivisionError as identifier:
+            pass
+
+    stream.close()
+    p.terminate()
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(p.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(all))
+    waveFile.close()
+
+    in_file = WAVE_OUTPUT_FILENAME
+
+    data, rate = sf.read(in_file)
+
+    #data = b''.join(all)
+    in_sec = len(data) / RATE
     for i in range(int(in_sec)):
         X = fftpack.fft(data[i*44100:(i+1)*44100])
-        freqList = fftpack.fftfreq(44100, d=1.0/ rate)
+        freqList = fftpack.fftfreq(44100, d=1.0/ RATE)
         
         amplitude = [np.sqrt(c.real ** 2 + c.imag ** 2) for c in X]
         in_freq = amplitude.index(max(amplitude))  #1番大きい振幅を拾ってるだけ(雑音出たら、たぶん上手くいかない)
